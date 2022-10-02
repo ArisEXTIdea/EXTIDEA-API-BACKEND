6f576a3297ec6ccfc3f1b23eab2b9890ca112b1e
Muhammad Aris Widaryani <arisk697@gmail.com>
2022-10-02 15:48:29 +0700

    update

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'extideam_extidea',
-        'password' => 'EXTbisnis123',
+        'username' => '',
+        'password' => '',
         'database' => '',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
